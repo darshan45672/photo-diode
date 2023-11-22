908fdf8b877016030245c8636cc510ffd6b6121d
darshan45672 <drshnbhandary@gmail.com>
2023-11-22 09:27:45 +0530

    added functionality to the canvas

--- js/script.js
+++ js/script.js
@@ -117,6 +117,16 @@ ctx.font = "bold small-caps 20px Arial";
 ctx.textBaseline = "middle";
 ctx.fillText("V", 245, 283)
 
+
+function voltageDisplay(volt) {
+  ctx.fillStyle = "white";
+  ctx.fillRect(170,269,55,30);
+  ctx.fillStyle = "black";
+  ctx.font = "bold small-caps 20px Arial";
+  ctx.textBaseline = "middle";
+  let text = `${volt} V`
+  ctx.fillText(text, 177, 285)
+}
 //power supply
 
 ctx.beginPath();
@@ -193,6 +203,151 @@ ctx.font = "bold small-caps 20px Arial";
 ctx.textBaseline = "middle";
 ctx.fillText("ammeter", 160, 465)
 
+function placeObjectoff(ctx, x, y, radius) {
+  // Draw a static circle at a specified position and radius
+  ctx.beginPath();
+  ctx.strokeStyle = 'black'; // Circle color
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(389, 285);
+  ctx.lineTo(389, 297);
+
+  ctx.moveTo(400, 285);
+  ctx.lineTo(400, 297);
+
+  ctx.moveTo(389, 297);
+  ctx.lineTo(400, 297);
+
+  ctx.moveTo(389, 293);
+  ctx.lineTo(400, 293);
+
+  ctx.moveTo(389, 290);
+  ctx.lineTo(400, 290);
+
+  ctx.moveTo(389, 272);
+  ctx.lineTo(392, 286);
+
+  ctx.moveTo(400, 272);
+  ctx.lineTo(397, 286);
+
+  ctx.moveTo(400, 274);
+  ctx.lineTo(394, 272);
+
+  ctx.moveTo(394, 274);
+  ctx.lineTo(389, 272);
+  
+  ctx.stroke();
+
+  ctx.fillStyle = 'black';
+  ctx.font = "bold small-caps 12px Arial";
+  ctx.fillText('Light source', 347, 238);
+
+  ctx.fillStyle = 'black';
+  ctx.font = "bold small-caps 18px Arial";
+  ctx.fillText('OFF', 380, 318);
+}
+
+placeObjectoff(ctx, 394, 273, 14);
+
+function placeObject(distance) {
+ 
+  ctx.clearRect(340, 0, canvas.width, canvas.height);
+
+  var startX =400; // X-coordinate of the starting point
+  var startY = canvas.height / 1.65; // Y-coordinate of the starting point
+
+  var objectx = startX + distance*4; // Calculate object's X-coordinate
+  var objectY = startY - 20; // Y-coordinate for the object above the line
+
+  // Draw line below the object
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(objectx, startY);
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 2;
+  ctx.stroke();
+
+  drawLightBulb(ctx, objectx, objectY); // Call function to draw light bulb
+ 
+  // Display distance text below the object
+  ctx.fillStyle = 'black';
+  ctx.font = '12px Arial';
+  ctx.fillText(distance + ' cm', objectx - 25, objectY + 40);
+}
+
+function drawLightBulb(ctx, x, y) {
+  // Draw small yellow circles as a light bulb shape
+  ctx.strokeStyle = 'black'; // Light bulb color
+  ctx.fillStyle = 'yellow';
+  ctx.lineWidth = 4;
+  // Draw small circles
+  ctx.beginPath();
+  ctx.arc(x - 7, y-10, 13, 0, Math.PI * 2);
+  ctx.stroke();
+  ctx.fill();
+
+  // Draw lines below the circles
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(x - 13, y + 3);
+  ctx.lineTo(x - 13, y + 15);
+  ctx.moveTo(x - 3, y + 3);
+  ctx.lineTo(x - 3, y + 15);
+  ctx.moveTo(x - 3, y + 15);
+  ctx.lineTo(x - 13, y + 15);
+  ctx.stroke();
+
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(x - 3, y + 10);
+  ctx.lineTo(x - 13, y + 10);
+  ctx.moveTo(x - 3, y + 7);
+  ctx.lineTo(x - 13, y + 7);
+  ctx.stroke();
+
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(x - 10, y + 3);
+  ctx.lineTo(x - 13, y -10);
+  ctx.moveTo(x -6, y + 3);
+  ctx.lineTo(x - 3, y -10);
+
+  ctx.lineWidth = 1;
+  ctx.moveTo(x - 13, y -10);
+  ctx.lineTo(x-9, y -8);
+  ctx.moveTo(x -9, y -10);
+  ctx.lineTo(x - 5, y -8);
+
+  ctx.stroke();
+
+  ctx.lineWidth = 2;
+  ctx.strokeStyle ='orange';
+  ctx.beginPath();
+  ctx.moveTo(x -24, y -8);
+  ctx.lineTo(x - 30, y -5);
+  ctx.moveTo(x -23, y -18);
+  ctx.lineTo(x - 29, y -20);
+  ctx.moveTo(x -18, y -26);
+  ctx.lineTo(x - 22, y -30);
+  ctx.moveTo(x -8, y -28);
+  ctx.lineTo(x - 8, y -36);
+  ctx.moveTo(x +2, y -25);
+  ctx.lineTo(x +6, y -31);
+  ctx.moveTo(x +10, y -18);
+  ctx.lineTo(x +15, y -21);
+  ctx.moveTo(x +11, y -8);
+  ctx.lineTo(x +17, y -5);
+  ctx.stroke();
+
+  ctx.fillStyle = 'black';
+  ctx.font = "bold small-caps 12px Arial";
+  ctx.fillText('Light source', x - 55, y - 45);
+}
 
 //Initialise system parameters here
 function varinit() {
@@ -209,11 +364,31 @@ function varinit() {
   //resistor 3
   $("#distance4Slider").slider("value", 0.01);
   $("#distance4Spinner").spinner("value", 0.01);
+
+  $("#distance1Slider").slider("disable");
+  $("#distance1Spinner").spinner("disable");
+  $("#distance2Slider").slider("disable");
+  $("#distance2Spinner").spinner("disable");
+  $("#distance3Slider").slider("disable");
+  $("#distance3Spinner").spinner("disable");
+  $("#distance4Slider").slider("disable");
+  $("#distance4Spinner").spinner("disable");
+  $("#distance4Spinner").spinner("disable");
+
+  $("#take-readings").prop("disabled", true);
+  $("#set-distance").prop("disabled", true);
+  $("#result-btn").prop("disabled", true);
+  $("#plot-graph-btn").prop("disabled", true);
+
+  $("#changing-text").text("Set Distance");
+  $("#message").text("Click on Start Simulation");
+
+  placeObjectoff(0);
 }
 
 function varchange() {
-  $("#distance1Slider").slider({ max: 300, min: 0, step: 10 });
-  $("#distance1Spinner").spinner({ max: 300, min: 0, step: 10 });
+  $("#distance1Slider").slider({ max: 100, min: 0, step: 1 });
+  $("#distance1Spinner").spinner({ max: 100, min: 0, step: 1 });
 
   $("#distance1Slider").on("slide", function (e, ui) {
     $("#distance1Spinner").spinner("value", ui.value);
@@ -301,51 +476,84 @@ function varupdate() {
   $("#distance2Spinner").spinner("value", $("#distance2Slider").slider("value"));
   $("#distance3Spinner").spinner("value", $("#distance3Slider").slider("value"));
   $("#distance4Spinner").spinner("value", $("#distance4Slider").slider("value"));
-  volt = $("#distance1Spinner").spinner("value"); //Updating variables
-  res1 = $("#distance2Spinner").spinner("value");
-  res2 = $("#distance3Spinner").spinner("value");
-  res3 = $("#distance4Spinner").spinner("value");
+  // d1 = $("#distance1Spinner").spinner("value"); //Updating variables
+  // d2 = $("#distance2Spinner").spinner("value");
+  // res2 = $("#distance3Spinner").spinner("value");
+  // res3 = $("#distance4Spinner").spinner("value");
+
+  if(count<=7){
+    d1 = $("#distance1Spinner").spinner("value");
+    placeObject(d1);
+  }
+  if (count >=8 && count <= 14) {
+    let d1 = $("#distance2Spinner").spinner("value");
+    placeObject(d1);
+  }
+  if(count >= 15 && count <= 21){
+    d3 = $("#distance3Spinner").spinner("value");
+    placeObject(d3);
+  }
+  if (count >= 22 && count <= 28) {
+    d4 = $("#distance4Spinner").spinner("value");
+    placeObject(d4);
+  }
 };
 
-function addtable(){
+function takeReadings(){
   count += 1;
-  switch (count) {
-    case 1:{
-      var table = document.getElementById("r2c2");
-      table.innerHTML = 22;
-    }
-      break;
+  $("#distance1Slider").slider("disable");
+  $("#distance1Spinner").spinner("disable");
+  if (count<=7) {
+    switch (count) {
+      case 1:{
+        var table = document.getElementById("r2c2");
+        table.innerHTML = 22;
+      }
+        break;
       case 2:{
-        var table = document.getElementById("r3c2");
-        table.innerHTML = 23;
+          var table = document.getElementById("r3c2");
+          table.innerHTML = 23;
       }
-      break;
+        break;
       case 3:{
-        var table = document.getElementById("r4c2");
-        table.innerHTML = 24;
+          var table = document.getElementById("r4c2");
+          table.innerHTML = 24;
       }
-        break;
+          break;
       case 4:{
-        var table = document.getElementById("r5c2");
-        table.innerHTML = 25;
-        }
-        break;
+          var table = document.getElementById("r5c2");
+          table.innerHTML = 25;
+      }
+          break;
       case 5:{
-            var table = document.getElementById("r6c2");
-            table.innerHTML = 26;
-          }
-        break;
+              var table = document.getElementById("r6c2");
+              table.innerHTML = 26;
+      }
+          break;
       case 6:{
-          var table = document.getElementById("r7c2");
-          table.innerHTML = 27;
+            var table = document.getElementById("r7c2");
+            table.innerHTML = 27;
       }
-      break;
+        break;
       case 7:{
-        var table = document.getElementById("r8c2");
-        table.innerHTML = 28;
+          var table = document.getElementById("r8c2");
+          table.innerHTML = 28;
       }
-      break;
+        break;
+    }
+  }
+  if (count == 7) {
+    alert("Set distance D2 and click on take readings")
+    $("#distance2Slider").slider("enable");
+    $("#distance2Spinner").spinner("enable"); 
+    $("#message").text("Set D2 by varying the slider Distance D2 and Click on take readings");
+  }
+
+  if (count >=8 && count <= 14) {
+    switch (count) {
       case 8:{
+        $("#distance2Slider").slider("disable");
+        $("#distance2Spinner").spinner("disable"); 
         var table = document.getElementById("r2c3");
         table.innerHTML = 32;
       }
@@ -380,7 +588,21 @@ function addtable(){
         table.innerHTML = 38;
       }
       break;
+      default:
+        break;
+    }
+  }
+  if (count == 14) {
+    $("#distance3Slider").slider("enable");
+    $("#distance3Spinner").spinner("enable"); 
+    $("#message").text("Set D3 by varying the slider Distance D3 and Click on take readings");
+    alert("Set distance D3 and click on take readings")
+  }
+  if (count >= 15 && count <= 21) {
+    switch(count){
       case 15:{
+        $("#distance3Slider").slider("disable");
+        $("#distance3Spinner").spinner("disable"); 
         var table = document.getElementById("r2c4");
         table.innerHTML = 42;
       }
@@ -415,7 +637,24 @@ function addtable(){
         table.innerHTML = 48;
       }
       break;
+
+      default:
+        break;
+    }
+  }
+
+  if (count == 21) {
+    alert("Set distance D4 and click on take readings")
+    $("#distance4Slider").slider("enable");
+    $("#distance4Spinner").spinner("enable"); 
+    $("#message").text("Set D4 by varying the slider Distance D4 and Click on take readings");
+  }
+
+  if (count >= 22 && count <= 28) {
+    switch(count){
       case 22:{
+        $("#distance4Slider").slider("disable");
+        $("#distance4Spinner").spinner("disable"); 
         var table = document.getElementById("r2c5");
         table.innerHTML = 52;
       }
@@ -450,22 +689,34 @@ function addtable(){
         table.innerHTML = 58;
       }
       break;
-      
-    default:
-      break;
+    }
+  }
+  if(count >= 28){
+    alert("Click on Plot graph to see the I-V characteristics")
+    $("#message").text("Click on Plot Graph to view the graph");
+    $("#take-readings").prop("disabled", true);
+    $("#plot-graph-btn").prop("disabled", false);
   }
 
 }
 
-function showvalue(){
-  var hiddenCells = document.querySelectorAll(".column-font");
-  hiddenCells.forEach(function(cell) {
-    cell.style.display = 'table-cell';
-  });
+function startSimulation() {
+  voltageDisplay(0);
+  alert("Set distance D1 and click on take readings")
+  $("#simulate-btn").prop("disabled", true);
+  $("#set-distance").prop("disabled", false);
+  $("#take-readings").prop("disabled", false);
+  $("#result-btn").prop("disabled", false);
+
+  $("#distance1Slider").slider("enable");
+  $("#distance1Spinner").spinner("enable");
+  $("#changing-text").text("Set Distance D1");
+  $("#message").text("Set D1 by varying the slider Distance D1 and Click on take readings");
 }
 
 function showResult() {
   document.getElementById('p3').style.display = 'block'   
+  document.getElementById('result-display-div').style.display = 'block'   
 }
 
 window.addEventListener("load", varinit);
